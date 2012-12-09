29fe3da4ab5adb52528cf1b366e5491caf3e8107
Sanqiang <zhaosanqiang916@gmail.com>
2012-12-09 14:08:35 -0500

    12/9/2012

---
+++ Q3-VerifyAnagram.h
@@ -0,0 +1,14 @@
+//
+//  Q113-VerifyAnagram.h
+//  Algorithm-Linux
+//
+//  Created by SanQ11iang Zhao on 12/3/12.
+//  Copyright (c) 2012 SanQ11iang Zhao. All rights reserved.
+//
+
+#ifndef __Algorithm_Linux__Q113_VerifyAnagram__
+#define __Algorithm_Linux__Q113_VerifyAnagram__
+
+#include <iostream>
+bool isAnagram(char *left, char *right);
+#endif /* defined(__Algorithm_Linux__Q113_VerifyAnagram__) */
